45ed06391fdb098ed2208c6b2f39f3f605ed8cd6
Programmer <Programmer@PROGRAMMER-PC>
2016-09-03 14:40:52 +0100

    Added a variable for remaining desktops for creation process.

--- MultiDesk/Globals.cs
+++ MultiDesk/Globals.cs
@@ -5,6 +5,7 @@ namespace MultiDesk
     class Globals
     {
         public static int NoDesktops = 6;
+        public static int RemainingDesktops = NoDesktops - 1;
         public static int ScreenHeight = Screen.PrimaryScreen.Bounds.Height;
         public static int ScreenWidth = Screen.PrimaryScreen.Bounds.Width;        
         public static int[] TopLeft = { 10, 10 };
