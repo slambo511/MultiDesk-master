beac502510e2f0ecf78b2529e7e3812a90240c8c
Programmer <Programmer@PROGRAMMER-PC>
2016-09-07 20:25:32 +0100

    saveit check for null

--- MultiDesk/RunningWindow.cs
+++ MultiDesk/RunningWindow.cs
@@ -224,8 +224,11 @@ namespace MultiDesk
             {
                 menuPosition[i] = Convert.ToInt32(EditRegistry.ReadKey(i.ToString()));
             }
-            ActiveForm.Left = menuPosition[0];
-            ActiveForm.Top = menuPosition[1];
+            if (ActiveForm != null)
+            {
+                ActiveForm.Left = menuPosition[0];
+                ActiveForm.Top = menuPosition[1];
+            }
         }
 
         private void RunningWindow_Activated(object sender, EventArgs e)
