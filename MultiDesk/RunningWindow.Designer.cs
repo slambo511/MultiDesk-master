e2c263520940ea2218a1797640ce6d63c12edb4e
Programmer <Programmer@PROGRAMMER-PC>
2016-09-08 09:42:03 +0100

    Resized RunningWindow to try and see how to stop large expansion.

--- MultiDesk/RunningWindow.Designer.cs
+++ MultiDesk/RunningWindow.Designer.cs
@@ -169,7 +169,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(419, 28);
+            this.ClientSize = new System.Drawing.Size(419, 30);
             this.ContextMenuStrip = this.contextMenuStrip1;
             this.Controls.Add(this.btnTen);
             this.Controls.Add(this.btnNine);
